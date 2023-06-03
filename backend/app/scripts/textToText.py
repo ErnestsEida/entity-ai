dcff3ad2a15dca09c41ea6498e19bacb9c89e14f
Leo <dadajevs@gmail.com>
2023-06-03 22:47:06 +0300

    tex to text

---
+++ backend/app/scripts/textToText.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import openai
+import torch
+import json
+import requests
+from dotenv import load_dotenv
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+# import
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+openai.api_key = API_KEY
+f = open("temp/inputTextToText.txt", "r")
+data = f.read()
+
+# ChatGPT convert
+API_URL = 'https://api-inference.huggingface.co/models/facebook/blenderbot-3B'
+headers = {"Authorization": f"Bearer {API_KEY}"}
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+response = query(data)
+
+# output
+l = open("temp/output.txt", "w")
+l.write(response['generated_text'])
+l.close()
+
+# delete input file
+f.close()
+os.remove("temp/inputTextToText.txt")
+
+
