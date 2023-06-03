ffd37777be8d11d6da70827b26098ccc84b0c2a0
Leo <dadajevs@gmail.com>
2023-06-03 23:41:51 +0300

    textToSpeech

---
+++ backend/app/scripts/textToSpeech.py
@@ -0,0 +1,33 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from gtts import gTTS
+
+# import
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+f = open("temp/input.txt", "r")
+data = f.read()
+
+# ChatGPT convert
+language = 'en'
+
+API_URL = 'https://api-inference.huggingface.co/models/facebook/blenderbot-3B'
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+def query(payload):
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+response = query(data)
+
+# output
+myAudio = gTTS(text = response['generated_text'], lang = language, slow = False)
+myAudio.save("temp/output.mp3")
+
+# delete input file
+f.close()
+os.remove("temp/input.txt")
+
